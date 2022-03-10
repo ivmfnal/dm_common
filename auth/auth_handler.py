de6bb4a80bbac52aae7eb6e1ae8439b019817fd5
Igor Mandrichenko <ivm@fnal.gov>
2022-03-10 15:37:32 -0500

    .

--- auth/auth_handler.py
+++ auth/auth_handler.py
@@ -1,10 +1,10 @@
 from webpie import Response, WPApp
 from ..util import to_str, to_bytes
 from .dbuser import BaseDBUser as DBUser
+from .base_handler import BaseHandler
+
 from urllib.parse import quote_plus, unquote_plus
 import hashlib, json
-
-from base_handler import BaseHandler
 
 class AuthAppMixin(object):
 
