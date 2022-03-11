6810bee5770d6a84e05e26de8e926c10d30aa562
Igor Mandrichenko <ivm@fnal.gov>
2022-03-11 06:45:52 -0500

    .

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,7 +1,6 @@
 from .auth_client import TokenAuthClientMixin, AuthenticationError
 from .auth_handler import AuthHandler
-from .base_server import BaseApp
-from .base_client import BaseHandler
+from .base_server import BaseApp, BaseHandler
 from .password_hash import password_hash, PasswordHashAlgorithm
 from .rfc2617 import digest_client, digest_server
 from .dbuser import BaseDBUser
